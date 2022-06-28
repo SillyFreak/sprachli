21b553f373922808842b0a733b59966f480db494
Clemens Koza <clemens.koza@gmx.at>
2022-06-28 18:35:41 +0200

    begin unit tests for parser

--- src/grammar/mod.rs
+++ src/grammar/mod.rs
@@ -1,11 +1,34 @@
 lalrpop_mod!(sprachli, "/grammar/sprachli.rs");
 
+use lalrpop_util::ParseError;
+use lalrpop_util::lexer::Token;
+
 pub use sprachli::*;
 
-#[test]
-fn sprachli() {
-    assert!(ExprParser::new().parse("22").is_ok());
-    assert!(ExprParser::new().parse("(22)").is_ok());
-    assert!(ExprParser::new().parse("((((22))))").is_ok());
-    assert!(ExprParser::new().parse("((22)").is_err());
+pub type Error<'a> = ParseError<usize, Token<'a>, &'static str>;
+pub type Result<'a, T> = std::result::Result<T, Error<'a>>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::ast;
+
+    #[test]
+    fn test_expr_parser() {
+        fn parse(s: &str) -> Result<ast::Expr> {
+            ExprParser::new().parse(s)
+        }
+
+        assert!(matches!(parse("22"), Ok(ast::Expr::Integer(22))));
+
+        assert!(matches!(parse("a"), Ok(ast::Expr::Identifier(id)) if id == "a"));
+        assert!(matches!(parse("b2"), Ok(ast::Expr::Identifier(id)) if id == "b2"));
+
+        assert!(matches!(parse("(22)"), Ok(ast::Expr::Integer(22))));
+        assert!(matches!(parse("((22))"), Ok(ast::Expr::Integer(22))));
+        assert!(matches!(parse("((22)"), Err(_)));
+
+        assert!(matches!(parse("{ 22 }"), Ok(ast::Expr::Block(_))));
+    }
 }
