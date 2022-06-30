46245bc0a9b4ec22ee22682b88bf9868acad7381
Clemens Koza <clemens.koza@gmx.at>
2022-06-30 18:05:19 +0200

    add if expressions

--- src/ast/expressions.rs
+++ src/ast/expressions.rs
@@ -5,10 +5,17 @@ pub enum Expression {
     Integer(i32),
     Identifier(String),
     Block(Box<Block>),
+    If(If),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Block {
     pub statements: Vec<Statement>,
     pub expression: Option<Expression>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct If {
+    pub then_branches: Vec<(Expression, Block)>,
+    pub else_branch: Option<Box<Block>>,
 }
