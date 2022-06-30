431e08f4418862e643618210b331cd047cdc550d
Clemens Koza <clemens.koza@gmx.at>
2022-06-30 15:31:37 +0200

    add some statement unit tests

--- src/grammar/mod.rs
+++ src/grammar/mod.rs
@@ -31,4 +31,23 @@ mod tests {
 
         assert!(matches!(parse("{ 22 }"), Ok(ast::Expr::Block(_))));
     }
+
+    #[test]
+    fn test_stmt_parser() {
+        fn parse(s: &str) -> Result<ast::Stmt> {
+            StmtParser::new().parse(s)
+        }
+
+        assert!(matches!(parse("22;"), Ok(ast::Stmt::Expr(ast::Expr::Integer(22)))));
+
+        assert!(matches!(parse("a;"), Ok(ast::Stmt::Expr(ast::Expr::Identifier(id))) if id == "a"));
+        assert!(matches!(parse("b2;"), Ok(ast::Stmt::Expr(ast::Expr::Identifier(id))) if id == "b2"));
+
+        assert!(matches!(parse("(22);"), Ok(ast::Stmt::Expr(ast::Expr::Integer(22)))));
+        assert!(matches!(parse("((22));"), Ok(ast::Stmt::Expr(ast::Expr::Integer(22)))));
+
+        assert!(matches!(parse("fn foo() {}"), Ok(ast::Stmt::Declaration(ast::Declaration::Fn(_)))));
+
+        assert!(matches!(parse("22"), Err(_)));
+    }
 }
