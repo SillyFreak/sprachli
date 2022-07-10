944b44aeca344151bbd11c360984dbabaafe4dc4
Clemens Koza <clemens.koza@gmx.at>
2022-07-10 21:57:19 +0200

    use lib.rs so that everything isn't marked as unused

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+#[macro_use] extern crate lalrpop_util;
+
+pub mod ast;
+pub mod fmt;
+pub mod grammar;
+pub mod interpreter;
+
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-#[macro_use] extern crate lalrpop_util;
-
-mod ast;
-mod fmt;
-mod grammar;
-mod interpreter;
-
 use std::env;
 use std::fs;
+
+use sprachli::grammar::SourceFileParser;
+use sprachli::interpreter::Interpreter;
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -24,34 +20,11 @@ fn main() {
 
     let source = fs::read_to_string(filename).unwrap();
 
-    let parser = grammar::SourceFileParser::new();
+    let parser = SourceFileParser::new();
     let ast = parser.parse(&source).unwrap();
 
-    let interpreter = interpreter::Interpreter::new();
+    let interpreter = Interpreter::new();
     let result = interpreter.visit_source_file(&ast).unwrap();
 
     println!("{result:?}");
-
-    // let exprs = [
-    //     "-1 * f(2)",
-    //     "if a { 1 } else if b { 2 }",
-    //     "{ 1; 2 }",
-    // ];
-    // for &expr in &exprs {
-    //     let expr = grammar::ExpressionParser::new().parse(expr)?;
-    //     println!("{:?}", expr);
-    //     println!("{:#?}", expr);
-    // }
-
-    // let sources = [
-    //     "pub fn foo(a, b) { 1 }",
-    //     "pub struct Foo;",
-    //     "pub struct Bar(a);",
-    //     "pub struct Baz { a }",
-    // ];
-    // for &source in &sources {
-    //     let source = grammar::SourceFileParser::new().parse(source)?;
-    //     println!("{:?}", source);
-    //     println!("{:#?}", source);
-    // }
 }
