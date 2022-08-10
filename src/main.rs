cb5878a068cd8f95389727fcf199837e2dcec4fa
Clemens Koza <clemens.koza@gmx.at>
2022-08-10 18:34:53 +0200

    implement CLI, except for inferring input kind (source/bytecode) and output filename (foo.spr --> foo.sprb)

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::fs;
 use clap::{ArgGroup, Parser};
 
 use sprachli::bytecode::{parser::parse_bytecode, Error as BytecodeError};
-use sprachli::compiler::write_bytecode;
-use sprachli::compiler::{Error as CompilerError, Module};
+use sprachli::compiler::{write_bytecode, Error as CompilerError, Module};
 use sprachli::parser::parse_source_file;
 use sprachli::vm::{Error as RuntimeError, Vm};
 
@@ -68,27 +67,106 @@ pub enum Error {
     Runtime(#[from] RuntimeError),
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum InputKind {
+    Source,
+    Bytecode,
+}
+
+fn derive_out_filename(_file: &str) -> Result<String, CompilerError> {
+    todo!();
+}
+
+fn derive_input_kind(_file: &str) -> Option<InputKind> {
+    todo!();
+}
+
+fn read_source_from_file(path: &str) -> Result<String, CompilerError> {
+    let source = fs::read_to_string(path)?;
+    Ok(source)
+}
+
+fn read_bytecode_from_file(path: &str) -> Result<Vec<u8>, CompilerError> {
+    let source = fs::read(path)?;
+    Ok(source)
+}
+
+fn compile_source(source: &str) -> Result<Module, CompilerError> {
+    let ast = parse_source_file(&source).map_err(CompilerError::from)?;
+    println!("{ast:#?}");
+
+    let module = Module::new(ast)?;
+    println!("{module:#?}");
+
+    Ok(module)
+}
+
+fn write_bytecode_to_file(path: &str, module: &Module) -> Result<(), CompilerError> {
+    let mut file = fs::File::create(path)?;
+    write_bytecode(&mut file, module)?;
+    Ok(())
+}
+
 fn main() -> Result<(), anyhow::Error> {
     use Args::*;
+    use InputKind::*;
 
     let args = Args::parse();
 
     match args {
-        Compile { .. } => todo!(),
-        Run { file, .. } => {
-            let source = fs::read_to_string(file).map_err(CompilerError::from)?;
+        Compile { file, out_file } => {
+            let out_file = match out_file {
+                Some(out_file) => out_file,
+                None => derive_out_filename(&file)?,
+            };
 
-            let ast = parse_source_file(&source).map_err(CompilerError::from)?;
-            println!("{ast:#?}");
+            let source = read_source_from_file(&file)?;
+            let module = compile_source(&source)?;
+            write_bytecode_to_file(&out_file, &module)?;
+            Ok(())
+        }
+        Run {
+            file,
+            source,
+            bytecode,
+            out_file,
+            output,
+        } => {
+            let kind = if source || output || out_file.is_some() {
+                Source
+            } else if bytecode {
+                Bytecode
+            } else {
+                derive_input_kind(&file).unwrap()
+            };
 
-            let module = Module::new(ast)?;
-            println!("{module:#?}");
+            let bytecode = match kind {
+                Source => {
+                    let out_file = match (out_file, output) {
+                        (Some(out_file), _) => Some(out_file),
+                        (None, true) => Some(derive_out_filename(&file)?),
+                        (None, false) => None,
+                    };
 
-            let mut bytes = Vec::new();
-            write_bytecode(&mut bytes, &module).map_err(CompilerError::from)?;
-            println!("{bytes:?}");
+                    let source = read_source_from_file(&file)?;
+                    let module = compile_source(&source)?;
 
-            let module = parse_bytecode(&bytes)?;
+                    if let Some(out_file) = out_file {
+                        write_bytecode_to_file(&out_file, &module)?;
+                    }
+
+                    let mut bytecode = Vec::new();
+                    write_bytecode(&mut bytecode, &module).map_err(CompilerError::from)?;
+                    bytecode
+                }
+                Bytecode => {
+                    read_bytecode_from_file(&file)?
+                }
+            };
+
+            println!("{bytecode:?}");
+
+            let module = parse_bytecode(&bytecode)?;
             println!("{module:#?}");
 
             let result = Vm::new(module).run()?;
